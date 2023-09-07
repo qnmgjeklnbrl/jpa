199e6409169af293b260c6b7778846937ee68647
qnmgjeklnbrl <rns0721@naver.com>
2023-09-07 22:27:22 +0900

    9/7 데이터베이스 연동 + CURD API 작성

---
+++ src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,64 @@
+package com.springboot.jpa.data.dao.impl;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.repository.ProductRepository;
+@Component
+public class ProductDAOImpl implements ProductDAO {
+
+    private final ProductRepository productRepository;
+    
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product insertProduct(Product product) {
+        Product saveProduct = productRepository.save(product);
+        return saveProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+        Product selectedProduct = productRepository.getById(number);
+        return selectedProduct;
+    }
+
+    @Override
+    public Product updateProduct(Long number, String name) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+        Product updatedProduct;
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdatedAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+        return updatedProduct;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+        
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
+    }
+    
+}
---
+++ src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -0,0 +1,77 @@
+package com.springboot.jpa.data.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+    public Long getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return this.stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    
+}
